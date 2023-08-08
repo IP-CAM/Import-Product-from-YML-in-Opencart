f75eb7b1c5b9fbf300d9f9ccb791cc9d0d4f492f
Роман Ченский <romaleg.sky@yandex.ru>
2023-08-08 12:50:14 +0300

    Добавил пару проверок и новую модель, а теперь пойду кушать

--- auto-updates.php
+++ auto-updates.php
@@ -49,23 +49,58 @@ $config->set('config_language_id', 1);
 $loader->model('catalog/product');
 $model = $registry->get('model_catalog_product');
 
+// тут загружаем другую модель, написанную ранее, так как требует меньше данных для обновления
+$loader->model('tool/supplier');
+$model_supplier = $registry->get('model_tool_supplier');
+
  
 $data = simplexml_load_file('https://humidorpro.ru/yandex-yml.xml');
 
 $i = 0;
 $k = 0;
+$j = 0;
 foreach ($data->shop->offers->offer as $row) {
 
     $data['filter_model'] = $row->sku;
     $result = $model->getProducts($data);
     $k ++;
 
+    $find = 0;
+    $product_id = 0;
+    $price = 0;
+    $name = '';
 
-    if(!count($result)) {
+    // дополнительная проверка, так как модель позвращает не полное сходство, а вхождение
+    foreach($result as $product) {
+        if ($product['model'] == $row->sku) {
+            $find = 1;
+            $product_id = $product['product_id'];
+            $price = $product['price'];
+            $name = $product['name'];
+        }
+    }
+
+
+    if(!$find) {
         echo 'Товара нет: ' . $row->name;
         echo ' (' .  getCategory($row->categoryId, $data) . ')';
         $i++;
         echo ' - Нужно добавить: ' . $i . ' товаров' .PHP_EOL;
+    } else {
+        $old_price = intval($price);
+        $new_price = generatePrice($row->price);
+        if($new_price != $old_price) {
+            $j++;
+            echo 'Обновление цены: ' . $name . ' "' . $row->sku .  '"';
+            echo ' (с ' . $old_price . ' на ' . $new_price . ') ';
+            echo ' - Нужно обновить: ' . $j . ' товаров' .PHP_EOL;
+            $data['model'] = $row->sku;
+            $data['price'] = $new_price;
+            $data['quantity'] = 20;
+ 
+            // $model_supplier->editProduct($product_id, $data);
+            
+        }
     }
 }
 
@@ -73,6 +108,14 @@ foreach ($data->shop->offers->offer as $row) {
 
 echo 'Всего товаров: ' . $k;
 
+
+function generatePrice($supplier_price) : int {
+    if(intval($supplier_price) < 6000) {
+        return $supplier_price * 2;
+    } else {
+        return  $supplier_price * 1.5;
+    }
+}
 
 
 function getCategory($category_id, $data, $category_name = '') : string {
